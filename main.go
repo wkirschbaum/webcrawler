115e040438b75baa1e5d9f0d0a342f05362a785e
Wilhelm Kirschbaum <wkirschbaum@gmail.com>
2014-06-25 10:38:17 +0200

    handle fetched data by writing it to file

--- main.go
+++ main.go
@@ -1,15 +1,43 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"os"
 
 	"github.com/unboxed/webcrawler/crawler"
 	"github.com/unboxed/webcrawler/fetcher"
 )
 
+func openLogFile(path string) *os.File {
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		panic("Unable to open the log file")
+	}
+	return f
+}
+
 func handleFetchedData(ch chan fetcher.FetchedResult) {
+	logFileTheRest := openLogFile("rest.log")
+	defer logFileTheRest.Close()
+	logFile200 := openLogFile("200.log")
+	defer logFile200.Close()
+	logFile400 := openLogFile("400.log")
+	defer logFile400.Close()
+	logFile500 := openLogFile("500.log")
+	defer logFile500.Close()
+
 	for result := range ch {
-		fmt.Printf("got result: %s status: %d\n", result.Url, result.Status)
+		switch {
+		case result.Status > 199 && result.Status < 299:
+			log.SetOutput(logFile200)
+		case result.Status > 399 && result.Status < 499:
+			log.SetOutput(logFile400)
+		case result.Status > 499 && result.Status < 599:
+			log.SetOutput(logFile500)
+		default:
+			log.SetOutput(logFileTheRest)
+		}
+		log.Printf("got result: %s status: %d\n", result.Url, result.Status)
 	}
 }
 
